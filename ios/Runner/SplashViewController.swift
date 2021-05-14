01b3fc949e793b74687180cd2ec3a281bf213ce6
dkolp <daka.kolp01@gmail.com>
2021-05-14 18:08:00 +0300

    ios SplashViewController: refactoring

--- ios/Runner/SplashViewController.swift
+++ ios/Runner/SplashViewController.swift
@@ -2,18 +2,17 @@ import UIKit
 import Lottie
 
 public class SplashViewController: UIViewController {
-    private var animationView: AnimationView?
-    
     public override func viewDidAppear(_ animated: Bool) {
-        animationView = .init(name: "splash_animation")
-        animationView!.frame = view.bounds
-        animationView!.contentMode = .center
-        animationView!.loopMode = .playOnce
-        animationView!.animationSpeed = 1.0
-        view.addSubview(animationView!)
-        animationView!.play {
+        let animationView: AnimationView = .init(name: "splash_animation")
+        animationView.frame = CGRect(origin: .zero, size: CGSize(width: 250.0, height: 250))
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+        animationView.animationSpeed = 2.0
+        animationView.play {
             (finished) in self.startFlutterApp()
         }
+        view.addSubview(animationView)
     }
     
     private func startFlutterApp() {
